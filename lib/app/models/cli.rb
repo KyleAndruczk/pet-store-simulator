e676752fa5e8ad1550571a8129ca3259957d69c4
Lindsay <lindsay.chen@yale.edu>
2021-06-25 01:14:50 +0800

    CLI

--- lib/app/models/cli.rb
+++ lib/app/models/cli.rb
@@ -10,6 +10,7 @@ class CLI
     @@chosen_store = nil
     @@resume = nil
     @@user = nil
+    @@boss_name
 
     # system('rake db:seed')
 
@@ -168,7 +169,7 @@ class CLI
         choosen_store_name_str = "#{chosen_store_arr[0]}" + " #{chosen_store_arr[1]}" + " #{chosen_store_arr[2]}" 
         # get the store owner's name
         boss_name = "#{chosen_store_arr[0]}"
-        boss_name = boss_name[0..((boss_name.length) - 3)]
+        @@boss_name = boss_name[0..((boss_name.length) - 3)]
         
         # get wage, remove dollar sign
         # avg_wage = "#{chosen_store_arr[5]}"
@@ -185,12 +186,19 @@ class CLI
             full_time = 1
         end
 
-        puts "#{boss_name}:  Congratulations, #{@@resume[:name]}, you're hired! Since you only have #{@@resume[:exp]} years of experience,\n\t you'll be payed half of our average salary and work 10 more hours than you asked,\n\t because, ya know, capitalism"
+        puts "#{@@boss_name}:  Congratulations, #{@@resume[:name]}, you're hired! Since you only have #{@@resume[:exp]} years of experience,\n\t you'll be payed half of our average salary and work 10 more hours than you asked,\n\t because, ya know, capitalism"
         puts "\nStarting Salary: $#{(avg_wage/2.0).to_i}\tHours Scheduled: #{(@@resume[:hours] + 10)}"
         # salary 
         
         @@user = Employee.add_to_db(@@resume[:name], @@resume[:exp], full_time, (@@resume[:hours] + 10), @@resume[:age], (avg_wage/2.0).to_i, store_obj.id)
-        puts @@user.pets_at_my_store
+        # for some reason using @@user.pets later was causing bugs
+        user_obj = Employee.all.find {|emp| emp.id == @@user.id}
+
+        5.times do 
+            Adoption.create(employee_id: user_obj.id, pet_id: Pet.ids.sample)
+        end
+        # sleep(8)
+        self.return_to_work
     end
 
     def self.start_work
@@ -234,37 +242,120 @@ class CLI
             puts table.render(:ascii)
             sleep(2)
             self.start_work
-        elsif selection == "Adopt a new pet"
+
+        end
+
+        if selection == "Adopt a new pet"
+        # self.adopt_a_new_pet
+            system('clear')
+            self.title
+            new_prompt = TTY::Prompt.new
+
+            puts "\n\nTime to get a new pet!\n\n"
+
+            selection_hash = new_prompt.collect do 
+                key(:species).ask("What species is the pet?")
+
+                key(:nickname).ask("What is the pet's nickname?")
+            end 
+
+            system('clear')
+            self.title
+        
+
+           new_pet = Pet.create(nickname: selection_hash[:nickname], species: selection_hash[:species], weight: rand(1.0..20.0).round(2), age: rand(1..20), alive: 1, years_in_captivity: 0, price: rand(1.5...100.0).round(2))
+            Adoption.create(employee_id: user_obj.id, pet_id: new_pet.id)
+
 
         
-        end 
+
+            self.print_boss_art
+            puts "#{@@boss_name}:  Alright, #{@@resume[:name]}, fine--I got that new pet you wanted. Its name is #{new_pet.nickname},\n\t and it's an #{new_pet.weight} lbs #{new_pet.age}-year-old #{new_pet.species}. \n\t And the $#{new_pet.price} it costed is coming out of YOUR paycheck!"
+            self.return_to_work
+        end
+
+        if selection == "Change my Schedule"
+            system('clear')
+            self.title
+            new_prompt = TTY::Prompt.new
+    
+            if user_obj.full_time 
+                work_status = "Full Time"
+            else
+                work_status = "Part Time"
+            end 
+    
+            puts "\n\nHours scheduled Form\n\nEmployee: #{@@resume[:name]}\n Hours this Week: #{user_obj.hours_scheduled}\n Status: #{work_status}\n"
+        
+            selection_hash = new_prompt.collect do 
+                key(:hrs).ask("How many hours do you want to work", convert: :int)
+    
+                key(:full_time).ask("Do you want to have full-time benefits?")
+            end 
+    
+            self.print_boss_art
+    
+            if selection_hash[:hrs] >= 80
+                full_time = rand(0.0..1.0).round
+                if full_time
+                    puts "#{@@boss_name}: Hmmm . . . #{selection_hash[:hrs]} is a lot. You can be full-time."
+                else
+                    # ptus
+                end 
+            end
+
+
+
+
+
+        end
+
+    
 
         if selection == "EWWWW! What's that smell?"
             user_obj.find_remove_dead_pets
         end
-
+    
         if selection == "Change what store I work at"
-            system('clear')
-            self.title
-            prompt = TTY::Prompt.new
-            @@chosen_store = prompt.select("What store do you want to change to?\n") do |menu|
-                Store.all.map do |store|
-                menu.choice "#{store.name.rjust(20)}" + "\n\tAverage Wage: $#{store.avg_wage}" + "\t |   Number of Employees: #{store.num_emps_at_store}\n"
-            end
+            # system('clear')
+            # self.title
+            # prompt = TTY::Prompt.new
+            # @@chosen_store = prompt.select("What store do you want to change to?\n") do |menu|
+            #     Store.all.map do |store|
+            #     menu.choice "#{store.name.rjust(20)}" + "\n\tAverage Wage: $#{store.avg_wage}" + "\t |   Number of Employees: #{store.num_emps_at_store}\n"
+            #     end
+                
+            # end
+            self.choose_store
+            self.get_hired
+            self.return_to_work
             
         end
-
-        end
-
+    
         if selection == "Quit my job!"
             Employee.destroy(user_obj.id)
-                puts "\nCongratulations, you have quit your job!"
-                sleep(2)
-                CLI.title_screen
+            puts "\nCongratulations, you have quit your job!"
+            sleep(2)
+            CLI.title_screen
         end
+    
+    
+       
+
+    
 
     end
 
+    def self.return_to_work
+        new_prompt = TTY::Prompt.new
+        selection = new_prompt.select("\n\nReturn when you're ready") do |option|
+            option.choice "Return to Work!" 
+        end 
+        
+        if selection == "Return to Work!"
+            self.start_work
+        end
+    end
 
     
 
